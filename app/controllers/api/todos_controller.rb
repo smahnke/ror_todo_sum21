1642514ac97778deae24c1d64642ce9c6900a993
Steph Mahnke <mahnkes1@msu.edu>
2021-06-15 19:23:43 -0600

    completed todo controller

--- app/controllers/api/todos_controller.rb
+++ app/controllers/api/todos_controller.rb
@@ -3,7 +3,36 @@ class Api::TodosController < ApplicationController
     render json: Todo.all
   end
 
-  def create
-
+  def show
+    render json: Todo.find(params[:id])
   end
+
+  def create
+    todo = Todo.new(todo_params)
+    if todo.save
+      render json: todo
+    else
+      render json: { errors: todo.errors}, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    todo = Todo.find(params[:id])
+    if todo.update(todo_params)
+      render json: todo
+    else 
+      render json: {errors: todo.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    Todo.find(params[:id]).destroy
+    render json: {message: 'todo deleted' }
+  end
+
+  private
+  # {todo: {title: "gsihgs", complete false}}
+    def todo_params
+      params.require(:todo).permit(:title, :complete)
+    end
 end
